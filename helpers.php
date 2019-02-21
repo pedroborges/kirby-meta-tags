d600ba0c8abb7c2ebdab5695b6b50f8a30951970
Pedro Borges <oi@pedroborg.es>
2019-02-21 17:56:21 -0300

    Add proper Page type

--- helpers.php
+++ helpers.php
@@ -1,15 +1,16 @@
 <?php
 
+use Kirby\Cms\Page;
 use PedroBorges\KirbyMetaTags\MetaTags;
 
 if (! function_exists('metaTags')) {
     /**
      * Generate meta tags for a given page.
      *
-     * @param  Page  $page
+     * @param  Kirby\Cms\Page  $page
      * @return MetaTags
      */
-    function metaTags(Page $page) : MetaTags
+    function metaTags(Page $page): MetaTags
     {
         return MetaTags::instance($page);
     }
